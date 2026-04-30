e3dd0933ec023719aa68f9b4be22e680f9e11122
Pablo Reis <pabloreis.pro@gmail.com>
2026-04-30 15:44:18 -0300

    Update download_kaggle.py

--- scripts/download_kaggle.py
+++ scripts/download_kaggle.py
@@ -3,7 +3,7 @@ import os
 os.makedirs("data", exist_ok=True)
 
 os.system(
-    "kaggle datasets download -d ricardomattos05/jogos-do-campeonato-brasileiro -p data/ --unzip"
+    "kaggle datasets download -d patelris/formula-1-complete-dataset-1950-2026 -p data/ --unzip"
 )
 
 print("Dataset atualizado com sucesso!")
